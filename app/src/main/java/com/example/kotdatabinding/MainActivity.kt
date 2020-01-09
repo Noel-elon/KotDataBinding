0c906456b9b4d202e2e08bd3c27979d01532ca5f
Noel-elon <noelnwaelugo@gmail.com>
2020-01-09 07:35:04 +0100

    View binding

--- app/src/main/java/com/example/kotdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/kotdatabinding/MainActivity.kt
@@ -6,24 +6,34 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.kotdatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        var doneBut : Button = findViewById(R.id.doneBUT)
-        doneBut.setOnClickListener { updateView(it) }
+
+
+       // var doneBut : Button = findViewById(R.id.doneBUT)
+        binding.doneBUT.setOnClickListener { updateView(it) }
     }
 
     private fun updateView(view: View) {
-        var nameTv = findViewById<TextView>(R.id.nameTV)
-        var nicknameEt = findViewById<EditText>(R.id.nicknameET)
+//        var nameTv = findViewById<TextView>(R.id.nameTV)
+  //      var nicknameEt = findViewById<EditText>(R.id.nicknameET)
+binding.apply {
+    nameTV.text = nicknameET.text
+    nicknameET.visibility = View.GONE
+    view.visibility = View.GONE
+    invalidateAll()
 
-        nameTv.text = nicknameEt.text
-        nicknameEt.visibility = View.GONE
-        view.visibility = View.GONE
+}
+
 
     }
 
